70bca78df9aee7c69ee47b3f087f7dee5cfb091a
Tarun-Chintapalli <tarun.chintapalli86@gmail.com>
2024-02-14 00:58:26 +0530

    Add files via upload

---
+++ nasubbu/script.js
@@ -0,0 +1,51 @@
+let yesButton = document.getElementById('yesButton');
+let noButton = document.getElementById('noButton');
+let smilingImage = document.querySelector('.smiling-image');
+
+let size = 1.5; // Initial size of the button
+let noButtonLabels = ["Really?", "Are you sure?", "This is sad"];
+let imageCount = 14; // Number of images
+let currentImageIndex = 1; // Initial image index
+
+function removeAllButtons() {
+    yesButton.remove();
+    noButton.remove();
+}
+
+noButton.addEventListener('click', () => {
+    size += 1; // Increase size by 0.1
+    yesButton.style.fontSize = `${size}rem`;
+
+    if (currentImageIndex+1 < imageCount) {
+        // Change label of NO button cyclically
+        let randomIndex = Math.floor(Math.random() * noButtonLabels.length);
+        noButton.innerText = noButtonLabels[randomIndex];
+
+        // Change image source cyclically
+        
+    } else {
+        // Remove NO button and update text
+        noButton.remove();
+        document.querySelector('h1').innerText = "ACCEPTTTT";
+    }
+    currentImageIndex = (currentImageIndex % imageCount) + 1;
+    smilingImage.src = `images/${currentImageIndex}.jpg`;
+});
+
+yesButton.addEventListener('click', () => {
+
+    removeAllButtons();
+
+    // Replace image with a GIF
+    smilingImage.src = 'images/accept.gif'; // Update with the path to your GIF
+    // Update text
+    document.querySelector('h1').innerText = " accept";
+
+    // Add download button for PDF
+    let downloadButton = document.createElement('a');
+    downloadButton.textContent = "💌";
+    downloadButton.href = "Love_letter.pdf"; // Update with the correct path to your PDF file
+    downloadButton.download = "valentine_card.pdf"; // Set the filename for the downloaded file
+    document.querySelector('.container').appendChild(downloadButton);
+    downloadButton.style.fontSize = '7rem';
+});
